e6a7692e279675e327bbd1d369c690fedb2a4333
John Wehr <johnwehr@gmail.com>
2020-11-25 13:38:18 -0500

    Add test for client reconnect handler, event

---
+++ tests/client-reconnect.test.js
@@ -0,0 +1,89 @@
+// @flow
+
+
+const uuid = require('uuid');
+const Client = require('@bunchtogether/braid-client');
+const Server = require('../src');
+const startWebsocketServer = require('./lib/ws-server');
+
+const port = 10000 + Math.round(Math.random() * 10000);
+
+jest.setTimeout(30000);
+
+describe('Client Reconnect', () => {
+  const credentialsValue = {
+    [uuid.v4()]: uuid.v4(),
+  };
+
+  test('Should call the reconnect handler and emit a reconnect event', async () => {
+    const ws = await startWebsocketServer('0.0.0.0', port);
+    const server = new Server(ws[0]);
+    const stopWebsocketServer = ws[1];
+    const client = new Client();
+    const openPromise = new Promise((resolve, reject) => {
+      server.once('open', resolve);
+      server.once('error', reject);
+    });
+    await client.open(`ws://localhost:${port}`, credentialsValue);
+    await openPromise;
+    const clientReconnectCredentialsPromise = new Promise((resolve, reject) => {
+      client.setReconnectHandler((credentials) => {
+        resolve(credentials);
+        return true;
+      });
+      client.once('error', reject);
+    });
+    const clientReconnectEventPromise = new Promise((resolve, reject) => {
+      client.once('reconnect', (isReconnecting: boolean) => {
+        if (isReconnecting) {
+          resolve();
+        } else {
+          reject(new Error('Client should reconnect when reconnect handler returns a value other than false'));
+        }
+      });
+      client.once('error', reject);
+    });
+    await server.close();
+    await expect(clientReconnectCredentialsPromise).resolves.toEqual(credentialsValue);
+    await clientReconnectEventPromise;
+    await client.close();
+    await stopWebsocketServer();
+    server.throwOnLeakedReferences();
+  });
+
+  test('Should not reconnect when the reconnect handler returns false', async () => {
+    const ws = await startWebsocketServer('0.0.0.0', port);
+    const server = new Server(ws[0]);
+    const stopWebsocketServer = ws[1];
+    const client = new Client();
+    const openPromise = new Promise((resolve, reject) => {
+      server.once('open', resolve);
+      server.once('error', reject);
+    });
+    await client.open(`ws://localhost:${port}`, credentialsValue);
+    await openPromise;
+    const clientReconnectCredentialsPromise = new Promise((resolve, reject) => {
+      client.setReconnectHandler((credentials) => {
+        resolve(credentials);
+        return false;
+      });
+      client.once('error', reject);
+    });
+    const clientReconnectEventPromise = new Promise((resolve, reject) => {
+      client.once('reconnect', (isReconnecting: boolean) => {
+        if (isReconnecting) {
+          reject(new Error('Client should not reconnect when reconnect handler returns false'));
+        } else {
+          resolve();
+        }
+      });
+      client.once('error', reject);
+    });
+    await server.close();
+    await expect(clientReconnectCredentialsPromise).resolves.toEqual(credentialsValue);
+    await clientReconnectEventPromise;
+    await client.close();
+    await stopWebsocketServer();
+    server.throwOnLeakedReferences();
+  });
+});
